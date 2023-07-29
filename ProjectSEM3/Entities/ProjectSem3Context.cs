8eb3b8bf67fd2fa5fb50a2db08a92cc85073d720
Long Nguyễn <long2112@Mac-mini-cua-Long.local>
2023-07-29 23:40:45 +0700

    server ip commit

--- ProjectSEM3/Entities/ProjectSem3Context.cs
+++ ProjectSEM3/Entities/ProjectSem3Context.cs
@@ -71,7 +71,7 @@ public partial class ProjectSem3Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=Project_SEM3 ;User ID=h4ckertr4shxd;Password=aBc#123aBc#321;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Data Source=113.190.48.76,46675;Initial Catalog=Project_SEM3 ;User ID=sa;Password=873058229a5b25c0c8747dc84e#b5d1a46f6a5;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
