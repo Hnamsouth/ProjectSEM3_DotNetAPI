b9831d893fecd50f3a1c49a88864a987259fb629
Nam Hoang <hnam17397@gmail.com>
2023-08-28 10:22:11 +0700

    1

--- ProjectSEM3/Entities/ProjectSem3Context.cs
+++ ProjectSEM3/Entities/ProjectSem3Context.cs
@@ -45,7 +45,11 @@ public partial class ProjectSem3Context : DbContext
 
     public virtual DbSet<UserInfo> UserInfos { get; set; }
 
+    public virtual DbSet<Voucher> Vouchers { get; set; }
     
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=SOUTH\\SQLEXPRESS;Initial Catalog=Projectsem3;Integrated Security=True;TrustServerCertificate=True");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Admin>(entity =>
